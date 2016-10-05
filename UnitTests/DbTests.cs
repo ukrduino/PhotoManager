33252a845927bd0528adb3cf65f766eb22d060c0
Sergey Romanyuk <Sergey.Romanyuk@dataart.com>
2016-10-05 11:44:39 +0300

    Unit test project added

---
+++ UnitTests/DbTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PhotoManager;
+using PhotoManager.Models;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class DbTests
+    {
+        [TestMethod]
+        public void CreateAlbumTests()
+        {
+            using (PhotoManagerDataModel dataModel = new PhotoManagerDataModel())
+            {
+                dataModel.Albums.Add(new AlbumModel { Title = "TestsAlbum1" });
+                dataModel.SaveChanges();
+            }
+        }
+        [TestMethod]
+        public void CreatePhotoTests()
+        {
+            using (PhotoManagerDataModel dataModel = new PhotoManagerDataModel())
+            {
+                dataModel.Photoes.Add(new PhotoModel { Title = "TestPhoto1" });
+                dataModel.SaveChanges();
+            }
+        }
+    }
+}
